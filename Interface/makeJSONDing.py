f342ae31eb81fbce535d58c920c9297879094388
KimberleySiemons <k.n.siemons@student.utwente.nl>
2021-05-30 19:34:11 +0200

    Filled in the database but not the tweets (Marnix)

--- Interface/makeJSONDing.py
+++ Interface/makeJSONDing.py
@@ -1,11 +1,11 @@
-types = ['short_sleeve_top', 'long_sleeve_top', 'short_sleeve_outwear', 'long_sleeve_outwear',
+Types = ['short_sleeve_top', 'long_sleeve_top', 'short_sleeve_outwear', 'long_sleeve_outwear',
 'vest', 'sling', 'shorts', 'trousers', 'skirt', 'short_sleeve_dress',
 'long_sleeve_dress', 'vest_dress', 'sling_dress']
 
-kleuren = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White", "Black", "Pink", "Brown"]
+Colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey", "White", "Black", "Pink", "Brown"]
 
-for type in types:
+for Type in Types:
     print('"' + type + '" : {')
-    for kleur in kleuren:
-        print('"' + kleur + '" : "",')
+    for Color in Colors:
+        print('"' + Color + '" : "",')
     print("}")
